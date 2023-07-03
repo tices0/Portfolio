f7e1f82c51be2eed6d8d7157c69861f27ee0d103
Tochi Davids <tochidavids18@gmail.com>
2023-07-03 18:48:16 +0100

    first outline added

--- src/index.js
+++ src/index.js
@@ -10,6 +10,25 @@ root.render(
   </React.StrictMode>
 );
 
+const polarToCartesian = (centerX, centerY, radius, angleInDegrees) => {
+  const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+};
+
+export const describeArc = (x, y, radius, startAngle, endAngle) => {
+  const start = polarToCartesian(x, y, radius, endAngle);
+  const end = polarToCartesian(x, y, radius, startAngle);
+  const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+  const d = [
+    "M", start.x, start.y,
+    "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+  ].join(" ");
+  return d;
+};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { describeArc } from "..";
+import { ReactComponent as GradientSvg } from "../components/svgGradient.svg";
+import { Svg1, Svg2 } from "../components/Svgs";
 
 function Home() {
 	return (
 		<main className="pages" id="home-page">
+			<GradientSvg />
 			<i className="fa-solid fa-circle"></i>
+			<div className="first-outline">
+				<Svg1 d={describeArc(0.5, 0.5, 0.48, -30, 30)} />
+				<Svg2 d={describeArc(0.5, 0.5, 0.48, 180 - 15, 180 + 15)} />
+			</div>
 			<div className="second-outline"></div>
 			<div className="scroll-down">
 				<div className="text">
